d8fd92c16d562f15f394ca097b252b5e317175cd
unknown <manueltaboada31@gmail.com>
2024-04-26 08:52:02 -0500

    modificando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { API_URL } from "../../env"
+
 
 function App() {
 
   return (
     <div>
-      <h1>Hola Linda {API_URL}</h1>
+      <h1>Hola Linda </h1>
       <p>Este es para ti mi amor</p>
     </div>
 
