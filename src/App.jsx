65f35d5602bafb527baa75e00253f5f9fc970009
unknown <manueltaboada31@gmail.com>
2024-04-26 08:38:47 -0500

    para desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { API_URL } from "../../env"
 
 function App() {
 
-
   return (
     <div>
-      <h1>Hola Linda</h1>
+      <h1>Hola Linda {API_URL}</h1>
+      <p>Este es el sitio en desarrollo</p>
     </div>
 
   )
